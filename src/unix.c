6fd15b00649b59172b9872821bb428ab45e661ab
Maciej Patucha <maciej.patucha@cfengine.com>
2012-07-13 10:55:41 +0200

    Add missing bracket.

--- src/unix.c
+++ src/unix.c
@@ -406,7 +406,7 @@ static const char *GetProcessOptions(void)
 int LoadProcessTable(Item **procdata)
 {
 #ifdef EXPERIMENTALLINUXPROC
-    if (!CollectLinuxProcInfo(procdata)
+    if (!CollectLinuxProcInfo(procdata))
     {
         return false;
     }
